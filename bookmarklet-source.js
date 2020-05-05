97fca486e91abe1e2b7e756228e21ee7b7154c16
Ryan Guill <ryanguill@gmail.com>
2020-05-04 21:07:20 -0500

    latest version with multiple selection, and adding a copy message to the screen

--- bookmarklet-source.js
+++ bookmarklet-source.js
@@ -1,65 +1,66 @@
+javascript: (function() {
+  let sel = document.getSelection();
+  let selText = "";
+  const reference = `[${document.title}](${location.href}) #[[Roam Capture]]`;
+  const selectedRanges = [];
 
-javascript: (
-	function () {
-		let sel = document.getSelection();
-		let selText = "";
-		const reference = `[${document.title}](${location.href}) #[[+Roam]]`;
-		const selectedRanges = [];
+  if (sel.rangeCount > 1) {
+    selText = reference + "\n";
+    //more than one thing selected
+    for (let i = 0; i < sel.rangeCount; i++) {
+      selText += `\t__${sel
+        .getRangeAt(i)
+        .toString()
+        .trim()}__ \n`;
+      selectedRanges.push(sel.getRangeAt(i));
+    }
+  } else {
+    selText = sel.toString().trim();
 
-		if (sel.rangeCount > 1) {
-			selText = reference + "\n";
-			//more than one thing selected
-			for (let i = 0; i < sel.rangeCount; i++) {
-				selText += `\t__${sel.getRangeAt(i).toString().trim()}__ \n`;
-				selectedRanges.push(sel.getRangeAt(i));
-			}
-		} else {
-			selText = sel.toString().trim();
+    //0 or 1 thing selected
+    if (selText.length) {
+      selectedRanges.push(sel.getRangeAt(0));
+      selText = `__${selText}__ via `;
+    }
+    selText += reference;
+  }
 
-			//0 or 1 thing selected
-			if (selText.length) {
-				selectedRanges.push(sel.getRangeAt(0));
-				selText = `__${selText}__ via `;
-			}
-			selText += reference;
-		}
+  const ta = document.createElement("textarea");
+  ta.textContent = `${selText}`;
 
+  document.body.appendChild(ta);
+  const docSel = document.getSelection();
+  docSel.removeAllRanges();
+  ta.select();
+  document.execCommand("copy");
+  docSel.removeAllRanges();
+  document.body.removeChild(ta);
 
-		const ta = document.createElement("textarea")
-		ta.textContent = `${selText}`;
+  //reselect the selected text
+  let newSel = document.getSelection();
+  for (let i = 0; i < selectedRanges.length; i++) {
+    newSel.addRange(selectedRanges[i]);
+  }
 
-		document.body.appendChild(ta);
-		const docSel = document.getSelection();
-		docSel.removeAllRanges();
-		ta.select();
-		document.execCommand("copy");
-		docSel.removeAllRanges();
-		document.body.removeChild(ta);
+  let toaster = document.createElement("div");
+  toaster.innerHTML = `Copied!`;
+  toaster.style.position = "fixed";
+  toaster.style.display = "block";
+  toaster.style.right = "10px";
+  toaster.style.top = "10px";
+  toaster.style.backgroundColor = "red";
+  toaster.style.color = "#FFFFFF";
+  toaster.style.padding = "5px";
+  toaster.style.borderRadius = "5px";
+  toaster.style.zIndex = "99999";
+  document.body.appendChild(toaster);
 
-		//reselect the selected text
-		let newSel = document.getSelection();
-		for (let i = 0; i < selectedRanges.length; i++) {
-			newSel.addRange(selectedRanges[i]);
-		}
+  setTimeout(function() {
+    toaster.style.opacity = 0;
+    toaster.style.transition = "opacity 2s";
+  }, 2000);
 
-		let toaster = document.createElement("div");
-		toaster.innerHTML = `Copied!`;
-		toaster.style.position = "absolute";
-		toaster.style.right = "10px";
-		toaster.style.top = "10px";
-		toaster.style.backgroundColor = "red";
-		toaster.style.color = "#FFFFFF";
-		toaster.style.padding = "5px";
-		toaster.style.borderRadius = "5px";
-		document.body.appendChild(toaster);
-
-		setTimeout(function() {
-			toaster.style.opacity = 0;
-			toaster.style.transition = "opacity 2s";
-		}, 2000);
-
-		setTimeout(function() {
-			document.body.removeChild(toaster);
-		}, 4000);
-	}
-)();
+  setTimeout(function() {
+    document.body.removeChild(toaster);
+  }, 4000);
+})();
