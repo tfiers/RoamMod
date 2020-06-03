2676c27660375f34e742f6ad6e4a202d510585e1
Tomas Fiers <tomas.fiers@gmail.com>
2020-06-03 20:41:19 +0200

    Extract showtime functionality to its own repo

--- roam_mod.js
+++ roam_mod.js
@@ -4,7 +4,6 @@ observer.observe(document, { childList: true, subtree: true })
 
 function modRoam() {
     applyAnnotationStyle()
-    showTiming()
 }
 
 function applyAnnotationStyle() {
@@ -13,27 +12,5 @@ function applyAnnotationStyle() {
         const parentDiv = tag.parentElement.parentElement.parentElement.parentElement.parentElement
         parentDiv.style.backgroundColor = '#fff4d0'
         parentDiv.style.color = '#555'
-    })
-}
-
-function showTiming() {
-    const infoDivs = document.querySelectorAll("div[data-edit-time]")
-    Array.from(infoDivs).forEach(div => {
-        // Remove prior styling
-        div.classList = []
-        div.style.backgroundImage = null
-        // Get created & last-edited times
-        function timestampToTime(attributeName) {
-            const ts = div.getAttribute(attributeName)
-            const d = new Date(parseInt(ts))
-            // This will be in browser locale's timezone
-            return d.toLocaleTimeString(navigator.language, 
-                {hour:"2-digit", minute:"2-digit"})
-        }
-        const createTime = timestampToTime("data-create-time")
-        const editTime = timestampToTime("data-edit-time")
-        // Add small text
-        div.innerText = `${createTime}\n${editTime}`
-        div.style.fontSize = "0.5em"
     })
 }
