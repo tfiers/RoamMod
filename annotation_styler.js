a02231a472b1bbdc85de928d4b306b3e8f56bb6c
Tomas Fiers <tomas.fiers@gmail.com>
2020-06-03 18:41:31 +0200

    Show block creation/edit times

--- annotation_styler.js
+++ annotation_styler.js
@@ -1,6 +1,11 @@
-const DOMMutationListener = _.throttle(applyAnnotationStyle, 200)
+const DOMMutationListener = _.throttle(hackRoam, 200)
 const observer = new MutationObserver(DOMMutationListener)
 observer.observe(document, { childList: true, subtree: true })
+
+function hackRoam() {
+    applyAnnotationStyle()
+    showTiming()
+}
 
 function applyAnnotationStyle() {
     const annotationTags = document.querySelectorAll('span.rm-page-ref[data-tag="n"]')
@@ -8,5 +13,27 @@ function applyAnnotationStyle() {
         const parentDiv = tag.parentElement.parentElement.parentElement.parentElement.parentElement
         parentDiv.style.backgroundColor = '#fff4d0'
         parentDiv.style.color = '#555'
+    })
+}
+
+function showTiming() {
+    const infoDivs = document.querySelectorAll("div[data-edit-time]")
+    Array.from(infoDivs).forEach(div => {
+        // Remove prior styling
+        div.classList = []
+        div.style.backgroundImage = null
+        // Get created & last-edited times
+        function timestampToTime(attributeName) {
+            const ts = div.getAttribute(attributeName)
+            const d = new Date(parseInt(ts))
+            // This will be in browser locale's timezone
+            return d.toLocaleTimeString(navigator.language, 
+                {hour:"2-digit", minute:"2-digit"})
+        }
+        // Add small text
+        const createTime = timestampToTime("data-create-time")
+        const editTime = timestampToTime("data-edit-time")
+        div.innerText = `${createTime}\n${editTime}`
+        div.style.fontSize = "0.5em"
     })
 }
